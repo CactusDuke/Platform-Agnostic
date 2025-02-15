b47b5096fb21d98890c53dc2cf8801fd08ff6ab3
Seth Close <seth@sethclose.au>
2025-02-15 14:27:24 -0700

    Python files init

--- src/setup.sh
+++ src/setup.sh
@@ -2,6 +2,11 @@
 sudo apt-get install postgresql -y
 sudo service postgresql start
 
-#Setting up
+#Setting up Postgres
 sudo -u postgres psql < src/createDB.sql;
+
+#Setting up
+rm -r venv
+python -m venv venv
+source venv/bin/activate
 
